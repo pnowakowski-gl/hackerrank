6bc56e4b21b042ccd71b659b58c8309ea560406f
Pawel Nowakowski <pawel.nowakowski@globallogic.com>
2022-10-18 15:42:58 +0200

    commiting 2 files

---
+++ validating_credit_card_number.py
@@ -0,0 +1,26 @@
+from re import search
+
+def card_number(number):
+    first_num = search(r"^[4-6]", number)
+    if not first_num:
+        return print("Invalid")
+    if len(number) != 16 and len(number) != 19:
+        return print("Invalid")
+    is_it = search(r"\d{4}-?\d{4}-?\d{4}-?\d{4}", number)
+    if is_it:
+        number = number.replace("-", "")
+        is_four_in_a_row = search(r"(\d)\1{3,}", number.replace("-", ""))
+        if is_four_in_a_row:
+            return print("Invalid")
+        else:
+            return print("Valid")
+    else:
+        return print("Invalid")
+card_number("5133-3367-8912-3456") # Invalid
+card_number("4123456789123456") # Valid
+card_number("5123-4567-8912-3456") # Valid
+card_number("61234-567-8912-3456") # Invalid
+card_number("4123356789123456") # Valid
+card_number("5123 - 3567 - 8912 - 3456") # Invalid
+card_number("2123356789123456") # Invalid
+card_number("4123356789121111") # Invalid
